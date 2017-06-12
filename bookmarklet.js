784283f7cd052b5787915dab7e8d8df60bce2331
lazywithclass <montecristoh@gmail.com>
2017-06-12 15:48:59 +0200

    first commit

---
+++ bookmarklet.js
@@ -0,0 +1,11 @@
+javascript: (() => {
+  // can I haz right click and copy?
+  $(document).off('contextmenu cut copy paste')
+
+  // CTRL+ALT runs tests
+  document.onkeydown = e => {
+    const evtobj = window.event? event : e
+    if (evtobj.keyCode == 13 && evtobj.ctrlKey) $('.bb-compile').click()
+  }
+})()
+
